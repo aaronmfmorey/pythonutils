565e7f735f393ce5ba4de5524895fcf2d47fe697
Aaron Morey <aaronmorey@gmail.com>
2025-09-03 07:36:20 -0500

    Add definitions to spoilers

--- pythonutils/wordlyzer.py
+++ pythonutils/wordlyzer.py
@@ -14,23 +14,22 @@ explore your options with Wordlyzer afterwards.
 
 To use, enter your current progress with letters in the places where you know the
 letters and underscores where you don't. Then enter the letters you've eliminated.
-Finally, enter whether you'd like to see spoilers. Seeing spoilers means the remaining
-words are printed out. No spoilers means it will just output a count of remaining
-words at the end.
-
-Limitation: I don't yet have a way of handling letters you know are in the puzzle
-but don't know the position of.
+Next enter the "Uncertain Letters", that is the letters that you know are included but
+don't know the exact location of (they're highlighted yellow in the game) Finally, enter
+whether you'd like to see spoilers. Seeing spoilers means the remaining words are printed
+out. "No Spoilers" means it will just output a count of remaining words at the end.
 """
 
 from english_dictionary.scripts.read_pickle import get_dict
 import re
 
-RE_FIVE_LETTERS_OR_UNDERSCORES = r'^[a-zA-Z_]{5}$'
+RE_FIVE_LETTERS_OR_UNDERSCORES = r"^[a-zA-Z_]{5}$"
+
 
 def convert_input_string_to_regex(input_string):
     """
-        Input: a string of exactly 5 characters. All characters must be either letters or underscores.
-        Returns: a regex string replacing the underscores with wildcards
+    Input: a string of exactly 5 characters. All characters must be either letters or underscores.
+    Returns: a regex string replacing the underscores with wildcards
     """
     if not bool(re.match(RE_FIVE_LETTERS_OR_UNDERSCORES, input_string)):
         raise ValueError("Input string must be exactly 5 characters")
@@ -40,42 +39,50 @@ def convert_input_string_to_regex(input_string):
 
     return re_string
 
+
 def get_yn_as_bool(yn_prompt):
     while True:
         yn_input = input(yn_prompt)
-        if len(yn_input) != 1 or yn_input.lower() not in ['y', 'n']:
+        if len(yn_input) != 1 or yn_input.lower() not in ["y", "n"]:
             print("Spoilers? must be either Y or N")
         else:
             return yn_input.lower() == "y"
+
 
 def prompt_for_valid_template_string(prompt):
     clarification = "Current Letters must be exactly 5 characters and only contain letters or underscores"
     while True:
         template_string = input(prompt)
 
-        if not bool(re.match(RE_FIVE_LETTERS_OR_UNDERSCORES, template_string)):  #len(template_string) != 5: # TODO AMM also check for characters
+        if not bool(re.match(RE_FIVE_LETTERS_OR_UNDERSCORES, template_string)):
             print(clarification)
         else:
             return template_string
 
+
 def get_inputs():
-    template_string = prompt_for_valid_template_string("Current letters:")
+    template_string = prompt_for_valid_template_string("Current letters: ")
     regex_string = convert_input_string_to_regex(template_string)
-    eliminated = set(input("Eliminated letters:"))
-    spoilers = get_yn_as_bool("Spoilers? [Y/N]:")
+    eliminated = set(input("Eliminated letters: "))
+    uncertain_letters = set(input("Uncertain Letters: "))
+    spoilers = get_yn_as_bool("Spoilers? [Y/N]: ")
 
-    return regex_string, eliminated, spoilers
+    return regex_string, eliminated, spoilers, uncertain_letters
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     english_dict = get_dict()
-    regex_string, eliminated, spoilers = get_inputs()
+    regex_string, eliminated, spoilers, uncertain_letters = get_inputs()
     match_count = 0
 
     for word in english_dict.keys():
         if len(word) == 5 and re.fullmatch(regex_string, word):
-            if set(word).isdisjoint(eliminated):
+            letter_set = set(word)
+            if letter_set.isdisjoint(eliminated) and (
+                uncertain_letters == () or letter_set.issuperset(uncertain_letters)
+            ):
                 if spoilers:
-                    print(word)
+                    print(f"{word}{english_dict[word]}")
                 match_count += 1
 
     print("Remaining possible words: " + str(match_count))
